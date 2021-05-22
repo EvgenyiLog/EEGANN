481a12ee945bc34b8dcd0dba10bd18c3c3481423
Evgenyi <evgenyi.logachev@gmail.com>
2021-05-22 18:56:40 +0300

    210521

--- decoding_xdawn_eeg.py
+++ decoding_xdawn_eeg.py
@@ -33,10 +33,24 @@ from keras.layers import Dense
 import keras
 from keras.layers import Input, Dense
 from keras.models import Model
+from keras.layers import Flatten
+from keras.layers.convolutional import Conv1D
+from keras.layers.convolutional import MaxPooling1D
+from keras.layers.embeddings import Embedding
+from keras.preprocessing import sequence
+from keras.layers import UpSampling1D
+
+from keras.models import Sequential
+from keras.models import Model
+from keras.layers import LSTM
+from keras.layers import Dense
+from keras.layers import RepeatVector
+from keras.layers import TimeDistributed
 
 
 
-print(__doc__)
+#print(__doc__)
+#with numpy==1.19.5
 
 data_path = sample.data_path()
 
@@ -48,15 +62,14 @@ tmin, tmax = -0.1, 0.3
 event_id = {'Auditory/Left': 1, 'Auditory/Right': 2,
             'Visual/Left': 3, 'Visual/Right': 4}
 n_filter = 3
-
 # Setup for reading the raw data
 raw = io.read_raw_fif(raw_fname, preload=True)
-print(raw)
+#print(raw)
 n_time_samps = raw.n_times
 time_secs = raw.times
 ch_names = raw.ch_names
 n_chan = len(ch_names)  # note: there is no raw.n_channels attribute
-print(n_chan)
+
 print('the (cropped) sample data object has {} time samples and {} channels.'
       ''.format(n_time_samps, n_chan))
 print('The last time sample is at {} seconds.'.format(time_secs[-1]))
@@ -152,7 +165,7 @@ raw_selection = raw[channel_index, start_sample:stop_sample]
 print(raw_selection)
 xb = raw_selection[1]
 yb = raw_selection[0].T
-plt.figure()
+plt.figure('Плохой')
 lines=plt.plot(xb, yb)
 plt.legend(lines,name)
 plt.grid()
@@ -165,22 +178,29 @@ yg = raw_selection[0].T
 
 
 
-yc=scipy.signal.convolve(yb,yg)
+yc=scipy.fft.ifft(scipy.fft.fft(yb)*scipy.fft.fft(yg))
 print(yc.shape)
-yc=np.resize(yc,yc.shape[0])
+#yc=np.resize(yc,yc.shape[0])
 
-plt.figure()
+plt.figure('Хороший')
 lines=plt.plot(xg, yg)
 plt.legend(lines,name)
 plt.grid()
 
-plt.figure()
+plt.figure('Взаимокорреляционная функция')
 lines=plt.plot(yc)
 
 plt.grid()
 
-t=np.linspace(0,len(yc)/fs,len(yc))
-x_train=np.column_stack((yc,t,t))
+
+print(epochs)
+ycn=yc/10
+ycn=np.where(np.abs(ycn)!=1,ycn,0)
+yc1=10*ycn
+ydc=np.real(scipy.fft.ifft(scipy.fft.fft(yc1)/scipy.fft.fft(yg)))
+'''
+t=np.linspace(0,len(ydc)/fs,len(ydc))
+x_train=np.column_stack((ydc,t,t))
 #x_train=keras.utils.normalize(x_train) 
 # this is the size of our encoded representations
 
@@ -189,22 +209,126 @@ encoding_dim = 3
 
 input = Input(shape=(3,))
 encoded = Dense(encoding_dim, activation='relu')(input)
-decoded = Dense(3, activation='linear')(encoded)
+decoded = Dense(3, activation='tanh')(encoded)
 
 # this model maps an input to its reconstruction
 autoencoder = Model(input, decoded)
 autoencoder.compile(optimizer='adadelta', loss='mse')
 
-autoencoder.fit(x_train, x_train,epochs=1000,batch_size=4)
+autoencoder.fit(x_train, x_train,epochs=500,batch_size=4)
 
-yc1,t1,t2=np.hsplit(autoencoder.predict(x_train), 3)
+ydcp,t1,t2=np.hsplit(autoencoder.predict(x_train), 3)
+#print(ydcp)
 
-plt.figure()
-plt.plot(yc)
+sequence = np.array(ydc[0:1000])
+# reshape input into [samples, timesteps, features]
+n_in = len(sequence)
+sequence = sequence.reshape((1, n_in, 1))
+# define model
+model = Sequential()
+model.add(LSTM(100, activation='relu', input_shape=(n_in,1)))
+model.add(RepeatVector(n_in))
+model.add(LSTM(100, activation='relu', return_sequences=True))
+model.add(TimeDistributed(Dense(1)))
+model.compile(optimizer='adam', loss='mse')
+# fit model
+model.fit(sequence, sequence, epochs=300, verbose=0)
+# connect the encoder LSTM as the output layer
+model = Model(inputs=model.inputs, outputs=model.layers[0].output)
+#plot_model(model, show_shapes=True, to_file='lstm_encoder.png')
+# get the feature vector for the input sequence
+yhat = model.predict(sequence)
+print(yhat.shape)
+yhat=yhat.reshape(yhat.shape[1])
+print(yhat.shape)
+#print(yhat)
+t1=xg[0:1000]
+plt.figure('Prediction')
+plt.plot(t1,yhat)
+plt.grid()
+'''
+
+seq_in = np.array(ydc[0:100])
+in1=seq_in
+print(in1.shape)
+# reshape input into [samples, timesteps, features]
+n_in = len(seq_in)
+seq_in = seq_in.reshape((1, n_in, 1))
+# prepare output sequence
+seq_out = seq_in[:, 1:, :]
+n_out = n_in - 1
+# define encoder
+visible = Input(shape=(n_in,1))
+encoder = LSTM(100, activation='elu')(visible)
+# define reconstruct decoder
+decoder1 = RepeatVector(n_in)(encoder)
+decoder1 = LSTM(100, activation='relu', return_sequences=True)(decoder1)
+decoder1 = TimeDistributed(Dense(1))(decoder1)
+# define predict decoder
+decoder2 = RepeatVector(n_out)(encoder)
+decoder2 = LSTM(100, activation='selu', return_sequences=True)(decoder2)
+decoder2 = TimeDistributed(Dense(1))(decoder2)
+# tie it together
+model = Model(inputs=visible, outputs=[decoder1, decoder2])
+model.compile(optimizer='Adam', loss='mse')
+model.summary()
+#plot_model(model, show_shapes=True, to_file='composite_lstm_autoencoder.png')
+# fit model
+model.fit(seq_in, [seq_in,seq_out], epochs=300, verbose=0)
+# demonstrate prediction
+yhat = model.predict(seq_in, verbose=0)
+out1=np.asarray(yhat[0])
+out1=out1.reshape(in1.shape[0])
+in1=in1.reshape(in1.shape[0])
+t1=xg[0:100]
+plt.figure('Prediction')
+plt.plot(t1,out1)
+plt.grid()
+
+plt.figure('Разность предсказанного...')
+plt.plot(t1,np.subtract(in1,out1))
+plt.grid()
+
+plt.figure('in')
+plt.plot(t1,in1)
 plt.grid()
 
 
 
+print(epochs)
+plt.figure('Сигнал после фильтрации')
+plt.plot(xg[0:1000],ydc[0:1000])
+plt.grid()
+
+axis=0
+ddof=0
+a = np.asanyarray(ydc)
+m = a.mean(axis)
+sd = a.std(axis=axis, ddof=ddof)
+x=np.std(a)
+y=np.amax(a)
+p=20*np.log10(y/x)
+print('SNR after filtration')
+print(p)
+print()
+
+axis=0
+ddof=0
+a = np.asanyarray(yb)
+m = a.mean(axis)
+sd = a.std(axis=axis, ddof=ddof)
+x=np.std(a)
+y=np.amax(a)
+p=20*np.log10(y/x)
+print(' SNR bad signal')
+print(p)
+print()
+
+dydc=np.subtract(yb,ydc)
+plt.figure('Разность...')
+plt.plot(xg,dydc)
+plt.grid()
+#ydc=scipy.signal.deconvolve(signal=yc,divisor=window)
 
 plt.show()
 
